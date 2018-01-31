15015873503916661a32b178bf43027726cf5f19
Austin Harman <peculiarcarrot@gmail.com>
2018-01-30 19:32:09 -0500

    Make time ability speed back up at a slower speed, changed the credits music to wind because creepy

--- Assets/Scripts/Abilities/TimeAbility.cs
+++ Assets/Scripts/Abilities/TimeAbility.cs
@@ -27,7 +27,7 @@ public class TimeAbility : Ability {
 	{
 		if(timeAlive > duration - Time.timeScale)
 		{
-			SetTimeScale(Mathf.Lerp(Time.timeScale, 1, .03f));
+			SetTimeScale(Mathf.Lerp(Time.timeScale, 1, .01f));
 		}
 	}
 
